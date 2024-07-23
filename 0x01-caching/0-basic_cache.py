8262e2ae2d33a2ce30136099bd41e32b2f684ebf
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-23 20:51:45 +0000

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,20 @@
+#!/bin/usr/env python3
+'''task 0 '''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''inherts from Base_Caching'''
+    MAX_LIMIT = None
+    
+    
+    def put(self, key, item):
+        '''assign value'''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self,key):
+        '''return value'''
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
---
+++ 0x01-caching/README.md
@@ -0,0 +1,6 @@
+project about cashing 
+Cache replacement policies - FIFO
+Cache replacement policies - LIFO
+Cache replacement policies - LRU
+Cache replacement policies - MRU
+Cache replacement policies - LFU
