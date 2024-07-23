a4d4a49c024b437be7bfdbd82ebc532336929e50
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-23 20:54:57 +0000

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,6 +16,4 @@ class BasicCache(BaseCaching):
 
     def get(self,key):
         '''return value'''
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
