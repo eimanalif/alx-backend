4553c701ea9fcb96070bc2b22ff4263526e11377
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-24 06:50:23 +0000

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,7 +8,7 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         '''assign value'''
-        if key is None and item is None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
 
