b33bcec48a94f9890c26ebe7f5d8e63e78690ff1
eimanalif <140444071+eimanalif@users.noreply.github.com>
2024-07-30 21:23:56 +0000

    task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
 ''' basic flask app'''
-from flask import Flask, render_template
 from flask_babel import Babel
+from flask import Flask, render_template
 
-app = Flask(__name__)
 
-
-class config:
+class Config:
+    """Represents a Flask Babel configuration"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object(config)
+app = Flask(__name__)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
-
 babel = Babel(app)
 
 
 @app.route('/')
-def get_index():
-    '''index page'''
+def get_index() -> str:
+    """index page"""
     return render_template('1-index.html')
 
 
